80584013bcd08140fd02edf352bde852e784f282
includealex <mishin.aa@phystech.edu>
2022-05-17 22:53:58 +0300

    rabbits changes

--- includes/game.h
+++ includes/game.h
@@ -1,16 +1,20 @@
 #ifndef INCLUDES_GAME_H
 #define INCLUDES_GAME_H
 
-#include <vector>
 #include <list>
+#include <random>
+#include <vector>
 
 class Game {
  public:
   std::vector<std::pair<size_t, size_t>> getcoords();
   void add_rabbit(std::pair<size_t, size_t> coord);
+  void add_rabbits(std::vector<std::pair<size_t, size_t>> arr);
   void kill_rabbit(std::pair<size_t, size_t> coord);
+  void addRabbits(size_t num, size_t max_x, size_t max_y);
 
  private:
+  std::pair<size_t, size_t> genrand_coord(size_t max_x, size_t max_y);
   std::list<std::pair<size_t, size_t>> rabbits_;
 };
 
--- sources/game.cpp
+++ sources/game.cpp
@@ -4,6 +4,12 @@ void Game::add_rabbit(std::pair<size_t, size_t> coord) {
   rabbits_.push_back(coord);
 }
 
+void Game::add_rabbits(std::vector<std::pair<size_t, size_t>> arr) {
+  for (auto el : arr) {
+    rabbits_.push_back(el);
+  }
+}
+
 std::vector<std::pair<size_t, size_t>> Game::getcoords() {
   std::vector<std::pair<size_t, size_t>> res;
 
@@ -15,4 +21,39 @@ std::vector<std::pair<size_t, size_t>> Game::getcoords() {
 
 void Game::kill_rabbit(std::pair<size_t, size_t> coord) {
   rabbits_.remove(coord);
+}
+
+void Game::addRabbits(size_t num, size_t max_x, size_t max_y) {
+  std::vector<std::pair<size_t, size_t>> fluffies;
+
+  auto find_el = [](std::vector<std::pair<size_t, size_t>> vec, std::pair<size_t, size_t> elem) {
+    for (auto el : vec) {
+      if (el == elem)
+        return true;
+    }
+
+    return false;
+  };
+
+  for (size_t i = 0; i < num; ++i) {
+    std::pair<size_t, size_t> val = {0, 0};
+
+    do {
+      val = genrand_coord(max_y - 1, max_x - 1);
+    } while (find_el(fluffies, val));
+
+    add_rabbit(val);
+  }
+}
+
+std::pair<size_t, size_t> Game::genrand_coord(size_t max_x, size_t max_y) {
+  auto randgen = [](size_t max) {
+    std::random_device r;
+    std::default_random_engine el(r());
+    std::uniform_int_distribution<size_t> uniform_dist(2, max);
+
+    return uniform_dist(el);
+  };
+
+  return {randgen(max_x), randgen(max_y)};
 }
