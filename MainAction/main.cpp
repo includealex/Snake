c5543f4bf586a376f418f6bfac77df5595ce37a6
includealex <mishin.aa@phystech.edu>
2022-05-17 20:30:46 +0300

    main action

--- MainAction/main.cpp
+++ MainAction/main.cpp
@@ -1,20 +1,16 @@
 #include <cstring>
 #include <iostream>
 
-#include "../Gview/gview.h"
-#include "../Tview/tview.h"
-#include "../View/view.h"
+#include "gview.h"
+#include "model.h"
+#include "tview.h"
+#include "view.h"
 
 int main(int argc, char* argv[]) {
-  std::string type;
-  if (argc == 1) {
-    type = "text";
-  } else {
-    type = argv[1];
-  }
+  std::string type = (argc == 1) ? "text" : argv[1];
 
   View* v = View::get(type);
   v->main_loop();
 
-  return 0;
+  delete v;
 }
